a37218e7000c49b786b1b95d8b2a6371a3ddf800
Jeferson Neves <62185462+jef771@users.noreply.github.com>
2021-11-16 20:32:35 -0300

    Add files via upload

---
+++ source_code_backend/src/main/java/fatec/grupodois/endurance/constant/Authority.java
@@ -0,0 +1,28 @@
+package fatec.grupodois.endurance.constant;
+
+public class Authority {
+
+    public static final String[] GUEST_AUTHORITIES = { "user:read",
+                                                        "evento: read"
+                                                     };
+    public static final String[] ORACLE_AUTHORITIES = { "user:read",
+                                                        "evento:create",
+                                                        "evento: read",
+                                                        "user:update"
+                                                      };
+    public static final String[] ADMIN_AUTHORITIES = {
+                                                        "user:read",
+                                                        "user:create",
+                                                        "user:delete",
+                                                        "user:update",
+                                                        "evento: read",
+                                                        "evento:create",
+                                                        "evento:delete",
+                                                        "evento:update",
+                                                        "fornecedor:read",
+                                                        "fornecedor:create",
+                                                        "fornecedor:delete",
+                                                        "fornecedor:update"
+                                                     };
+
+}
